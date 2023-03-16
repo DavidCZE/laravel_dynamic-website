09790f7e7b2c7f70dfde3b401aa92e2cfd9408bc
DavidCZE <david.malinek@gmail.com>
2023-03-16 18:11:07 +0100

    start of roles and permissions

--- Royal_Ocean/app/Http/Controllers/HomeController.php
+++ Royal_Ocean/app/Http/Controllers/HomeController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class HomeController extends Controller
 {
     //zobrazit home
     public function index() {
+
+        Permission::create(['name'=>'write post']);
         return view('home');
     }
 }
